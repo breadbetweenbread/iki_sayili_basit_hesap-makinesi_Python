8b0e616d5ec2f97ac85b0a6f5803df83428e6589
breadbetweenbread <110292564+breadbetweenbread@users.noreply.github.com>
2023-03-25 14:46:18 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,41 @@
+"""Hesap Makinesi Denemesi"""
+
+import sys
+
+sys.set_int_max_str_digits(999999999)
+
+num1 = int(input("İlk sayıyı giriniz:"))
+num2 = int(input("İkinci sayıyı giriniz:"))
+prcs = str(input("""Yapacağınız işlemi seçiniz;
+Toplama  (+)
+Çıkarma  (-)
+Çarpma   (*)
+Bölme    (/)
+Üst Alma (**)
+:"""))
+
+while True:
+    if prcs == "+":
+        print(f"{num1} + {num2} = {num1+num2}")
+        break
+    elif prcs == "-":
+        print(f"{num1} - {num2} = {num1-num2}")
+        break
+    elif prcs == "*":
+        print(f"{num1} * {num2} = {num1*num2}")
+        break
+    elif prcs == "/":
+        print(f"{num1} / {num2} = {num1/num2}")
+        break
+    elif prcs == "**":
+        print(f"{num1} ** {num2} = {num1**num2}")
+        break
+    else:
+        print("HATALI İŞLEM!!!")
+    prcs = str(input("""Yapacağınız işlemi seçiniz;
+Toplama  (+)
+Çıkarma  (-)
+Çarpma   (*)
+Bölme    (/)
+Üst Alma (**)
+:"""))
